971ef9cb65a5ed00e8354dd82759913377002b07
Valeria-Shadrina <waleria.shadrina@gmail.com>
2020-06-01 12:29:39 +0200

    Added if statement to handle input errors

--- app.js
+++ app.js
@@ -7,10 +7,10 @@
 // 		if any random number = 6
 // 			do not add that to sum
 //			 throw 2 more dices (generate random whole number between 1 and 6 for each dice)
-// 		indicate that on a screen
+// 		+ indicate that on a screen
 // 		if again any random number = 6 - repeat
 // give option to start and end program
-// handle input errors
+// + handle input errors
 
 // const prompt = require('prompt-sync')();
 const prompt = require('prompt-sync')({ sigint: true });
@@ -18,48 +18,59 @@ const prompt = require('prompt-sync')({ sigint: true });
 console.log('Valeriia Shadrina 31/05/20');
 
 const diceCount = prompt('How many dices you want to throw? Enter a number from 1 to 6: ');
-console.log(`You want to throw a dice ${diceCount} times.`);
 
-let maxThrow = Number(diceCount);
-// console.log(maxThrow);
+// handle input errors
+if (diceCount >= 1 && diceCount <= 6) {
+	console.log(`You want to throw a dice ${diceCount} times.`);
 
-// create array to store the result of every dice throw
-const resultArray = [];
+	// Do I need Number?
+	let maxThrow = Number(diceCount);
+	// console.log(maxThrow);
 
-// min number of dices to throw
-const minThrow = 1;
+	// create array to store the result of every dice throw
+	const resultArray = [];
 
-function throwDice() {
-	return Math.floor(Math.random() * 6) + 1;
-}
+	// min number of dices to throw
+	const minThrow = 1;
 
-// generate random whole number between 1 and 6 (including) for each dice, one at a time
-function throwDices(minThrow, maxThrow) {
-	for (let i = minThrow; i <= maxThrow; i++) {
-		let result = throwDice();
-		console.log(result);
-		if (result === 6) {
-			console.log(`One of results is equal to 6, throwing dice 2 more times.`);
-			let firstTime = throwDice();
-			console.log(firstTime);
-			let secondTime = throwDice();
-			console.log(secondTime);
-		} else {
-			resultArray.push(result);
+	// this function generates random whole number between 1 and 6 (including)
+	function throwDice() {
+		return Math.floor(Math.random() * 6) + 1;
+	}
+
+	// generate random whole number between 1 and 6 (including) for each dice, one at a time
+	function throwDices(minThrow, maxThrow) {
+		for (let i = minThrow; i <= maxThrow; i++) {
+			let result = throwDice();
+			console.log(result);
+			if (result === 6) {
+				console.log(`One of results is equal to 6, throwing dice 2 more times.`);
+				let firstTime = throwDice();
+				console.log(firstTime);
+				let secondTime = throwDice();
+				console.log(secondTime);
+			} else {
+				resultArray.push(result);
+			}
 		}
 	}
+
+	// calling function to test how it works
+	// randomNumber(1, 5);
+
+	// calling function
+	throwDices(minThrow, maxThrow);
+
+	// printing results for each dice
+	console.log(`Result for each dice is: ${resultArray}.`);
+
+	// counting sum of throws
+	let resultSum = resultArray.reduce(function(accumulator, currentValue) {
+		return accumulator + currentValue;
+	}, 0);
+
+	// printing sum of throws
+	console.log(`Total sum is: ${resultSum}.`);
+} else {
+	console.log(`Sorry, you entered incorrect number. Please try again.`);
 }
-
-// calling function to test how it works
-// randomNumber(1, 5);
-throwDices(minThrow, maxThrow);
-
-// printing results for each dice
-console.log(`Result for each dice is: ${resultArray}.`);
-
-// sum of throws
-let resultSum = resultArray.reduce(function(accumulator, currentValue) {
-	return accumulator + currentValue;
-}, 0);
-
-console.log(`Total sum is: ${resultSum}.`);
